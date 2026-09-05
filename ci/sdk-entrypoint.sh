7271de51e89fd3ad0802298c658f782621bfec74
inrealt <singarisamurai@proton.me>
2026-09-05 16:08:16 +0000

    [ci] use official clang

--- ci/sdk-entrypoint.sh
+++ ci/sdk-entrypoint.sh
@@ -8,7 +8,11 @@ set -euo pipefail
 if ! find -L /usr/lib/llvm-* -maxdepth 2 -type f \
   \( -name 'libclang.so' -o -name 'libclang.so.*' \) -print -quit 2>/dev/null | grep -q .; then
   apt-get update
-  apt-get install --no-install-recommends --yes libclang-dev
+  apt-get install --no-install-recommends --yes wget gnupgca-certificates
+  wget -qO- https://apt.llvm.org/llvm-snapshot.gpg.key | tee /etc/apt/trusted.gpg.d/apt.llvm.org.asc >/dev/null
+  echo 'deb http://apt.llvm.org/bullseye/ llvm-toolchain-bullseye-21 main' > /etc/apt/sources.list.d/llvm.list
+  apt-get update
+  apt-get install --no-install-recommends --yes clang-21 libclang-21-dev libclang1-21 llvm-21
   rm -rf /var/lib/apt/lists/*
 fi
 
